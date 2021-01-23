db31ffc236d419d3a43b146bf256f3c2373f2929
seanmyworkshop <sean.chen@myworkshop.ca>
2021-01-23 14:54:49 +0000

    add new file to playaround types

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-name = input("What is your name?")
-print("hello " + name + "!")
+name = input("What is your name?\n: ")
+print("hello \"" + name + "\"!")
+greeting = 'Hello!'
+greeting = "hello"
+greeting = """
+Hello
+new line
+third line
+            forth line
+"""
+print(greeting.upper() * 10)
---
+++ types.py
@@ -0,0 +1,20 @@
+a = 170
+print("Type of a: ", type(a))
+# convert int to string
+b = str(170)
+print("Type of b: ", type(b))
+
+c = "170"
+d = int(c)
+print("Type of d: ", type(d))
+
+f = True
+f = False
+
+nou = 1
+i = 0
+while nou < 100:
+  i = i + nou
+  nou = nou + 1
+
+print(i)
